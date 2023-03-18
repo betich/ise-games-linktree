07af4e9c4e39164965ad249409e12b35788f1353
betich <betich365@gmail.com>
2023-03-18 22:40:06 +0700

    fix: modify config

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -13,6 +13,7 @@ export default defineConfig({
       projectId: "svg89wzf",
       dataset: "production",
       apiVersion: "2023-03-18",
+      token: process.env.SANITY_TOKEN,
     }),
   ],
 });
